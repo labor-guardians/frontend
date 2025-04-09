8c58060c320dd651683cfaf4f279841942cccbb1
zziwonCHOI <wldnjssem6@naver.com>
2025-04-09 23:17:31 +0900

    button component

--- LawBot/src/App.jsx
+++ LawBot/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Button } from "./components/Button";
 
 function App() {
   return (
     <>
       <div className="bg-blue-500">test</div>
       <button className="btn">Default</button>
+      <Button text="등록"/>
     </>
   );
 }
