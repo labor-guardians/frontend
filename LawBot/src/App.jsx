2a9f456912c280b007ef93aa968a2abc838df0f7
zziwonCHOI <wldnjssem6@naver.com>
2025-04-11 02:12:23 +0900

    feat: mainpage 화면 구성

--- LawBot/src/App.jsx
+++ LawBot/src/App.jsx
@@ -2,20 +2,12 @@ import "./App.css";
 import { Button } from "./components/Button";
 import { InputText } from "./components/InputText";
 import { Nav } from "./components/Nav";
+import { MainPage } from "./pages/MainPage";
 
 function App() {
     return (
         <>
-            <Nav />
-
-            <div className="bg-blue-500">test</div>
-            <button className="btn">Default</button>
-            <InputText
-                label={"아이디"}
-                placeholder={"아이디를 입력해주세요"}
-                validationText={"아이디가 중복입니다. 비밀번호는 12자리 이상이어야합니다. "}
-            />
-            <Button text="등록" />
+        <MainPage/>
         </>
     );
 }
