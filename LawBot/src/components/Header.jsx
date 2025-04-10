d32f1c306e70e0a18459f9fcc53a586d049f3ff4
zziwonCHOI <wldnjssem6@naver.com>
2025-04-11 01:01:14 +0900

    feat: header component

---
+++ LawBot/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from "../assets/로고.png"
+import { Button } from './Button'
+export const Header = () => {
+  return (
+    <div className='p-10 flex-1' >
+        <div className='w-full h-auto flex flex- justify-between'>
+            <div class="flex felx-row">
+              <img src={logo} className='w-16 sm:w-16 md:w-18 lg:w-20 h-auto'/>
+              <div className="flex-col text-[#653F21] font-bold text-xl sm:text-2xl md:text-3xl ml-3 mt-3 font-['KanitBold']">
+                  <p>노동</p>
+                  <p>LawBot</p>
+              </div>
+            </div>
+            <Button text="login" text-xs sm:text-sm md:text-base lg:text-lg p-2 sm:p-3/>
+        </div>
+    </div>
+  )
+}
