e610b0f1ec027e9541575c369a14a0de02ca2cbb
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-04-13 19:45:31 +0900

    feat: login

---
+++ LawBot/src/pages/FindIdPassWord.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FindIdPassWord = () => {
+    return <div>FindIdPassWord</div>;
+};
--- LawBot/src/route/Router.jsx
+++ LawBot/src/route/Router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { MainPage } from "../pages/MainPage";
 import { Login } from "../pages/Login";
+import { SignUp } from "../pages/SignUp";
+import { FindIdPassWord } from "../pages/FindIdPassWord";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +12,8 @@ export const router = createBrowserRouter([
         children: [
             { index: true, element: <MainPage /> },
             { path: "/login", element: <Login /> },
+            { path: "/sign-up", element: <SignUp /> },
+            { path: "/find-id-password", element: <FindIdPassWord /> },
         ],
     },
 ]);
