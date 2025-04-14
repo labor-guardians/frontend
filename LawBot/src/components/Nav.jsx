6e263088aed8bb61a48a18bb50b70d5f7bf9f4ce
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-04-14 23:13:44 +0900

    feat: 헤더 반응형

--- LawBot/src/components/Nav.jsx
+++ LawBot/src/components/Nav.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Nav = () => {
     return (
-        <div className="w-fit h-fit flex justify-center items-center p-2 bg-stone-100 rounded-lg gap-2.5">
-            <div className="relative px-7.5 py-2 text-sm bg-stone-300 rounded-lg">챗봇</div>
+        <ul className="menu menu-vertical sm:menu-horizontal bg-base-200 rounded-box flex items-center gap-2.5">
+            <li className="relative">
+                <Link to="/chatbot" className="px-6">
+                    챗봇
+                </Link>
+            </li>
             <div className="h-2.5 w-[1px] bg-stone-300"></div>
-            <div className="relative px-7.5 py-2 text-sm">노무사</div>
+            <li>
+                <Link to="/labor-attorney-list" className="px-6">
+                    노무사
+                </Link>
+            </li>
             <div className="h-2.5 w-[1px] bg-stone-300"></div>
-            <div className="relative px-7.5 py-2 text-sm">채팅기록</div>
+            <li>
+                <Link to="/labor-attorney-chat-history" className="px-6">
+                    채팅기록
+                </Link>
+            </li>
             <div className="h-2.5 w-[1px] bg-stone-300"></div>
-            <div className="px-7.5 py-2 text-sm ">My</div>
-        </div>
+            <li>
+                <Link to="/mypage" className="px-6">
+                    마이페이지
+                </Link>
+            </li>
+        </ul>
     );
 };
