a14013a0ff222d874afa393df63f9842279dd871
zziwonCHOI <wldnjssem6@naver.com>
2025-04-15 00:21:48 +0900

    feat: 회원가인 회면 구성

--- LawBot/src/route/Router.jsx
+++ LawBot/src/route/Router.jsx
@@ -3,17 +3,19 @@ import App from "../App";
 import { MainPage } from "../pages/MainPage";
 import { Login } from "../pages/Login";
 import { SignUp } from "../pages/SignUp";
+import { SignUpLabor } from "../pages/SignUpLabor";
 import { FindIdPassWord } from "../pages/FindIdPassWord";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            { index: true, element: <MainPage /> },
-            { path: "/login", element: <Login /> },
-            { path: "/sign-up", element: <SignUp /> },
-            { path: "/find-id-password", element: <FindIdPassWord /> },
-        ],
-    },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <MainPage /> },
+      { path: "/login", element: <Login /> },
+      { path: "/sign-up", element: <SignUp /> },
+      { path: "/find-id-password", element: <FindIdPassWord /> },
+      { path: "/sign-up-labor", element: <SignUpLabor /> },
+    ],
+  },
 ]);
