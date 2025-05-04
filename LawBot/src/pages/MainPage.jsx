f115ac405b72cbf455c57f9c6c3c081626bdbcd2
zziwonCHOI <wldnjssem6@naver.com>
2025-05-04 23:56:26 +0900

    mainpage merge conflict 해결

--- LawBot/src/pages/MainPage.jsx
+++ LawBot/src/pages/MainPage.jsx
@@ -6,14 +6,18 @@ export const MainPage = () => {
   return (
     <>
       <Header />
-      <div className={"flex flex-col justify-center items-center mt-[25vh]"}>
-        <p className={"font-bold text-[3vw] text-center leading-relaxed"}>
+      <div className={"flex flex-col justify-center items-center mt-[40vh]"}>
+        <p
+          className={
+            "font-bold text-[6vw] sm:text-[4vw] md:text-[3vw] text-center leading-relaxed text-[#653F21]"
+          }
+        >
           개인을 위한 <br />
           노동 관련 법률 자문 챗봇
         </p>
         <Button
           text="Try LawBot ↗"
-          size="sm:w-[20vw] md:w-[15vw] lg:w-[10vw] mt-5"
+          size="w-full max-w-[200px] sm:max-w-[250px] md:max-w-[300px] text-sm sm:text-base md:text-lg mt-5"
         />
       </div>
     </>
