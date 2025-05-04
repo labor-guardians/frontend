62748c256d6cb35d9320ebb65db115cfe2271361
zziwonCHOI <wldnjssem6@naver.com>
2025-05-04 23:43:06 +0900

    chore: 버튼, 텍스트 크기 반응형 수정

--- LawBot/src/pages/MainPage.jsx
+++ LawBot/src/pages/MainPage.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Header } from '../components/Header'
-import { Button } from '../components/Button'
+import React from "react";
+import { Header } from "../components/Header";
+import { Button } from "../components/Button";
 
 export const MainPage = () => {
   return (
     <>
-    <Header/>
-    <div className={'flex flex-col justify-center mt-30 items-center'}>
-        <p className={'font-bold text-xl sm:text-2xl md:text-4xl text-center leading-relaxed'}>개인을 위한 <br/>노동 관련 법률 자문 챗봇</p>
-        <Button text="Try LawBot ↗" size="w-40 mt-5" />
-    </div>
+      <Header />
+      <div className={"flex flex-col justify-center items-center mt-[25vh]"}>
+        <p className={"font-bold text-[3vw] text-center leading-relaxed"}>
+          개인을 위한 <br />
+          노동 관련 법률 자문 챗봇
+        </p>
+        <Button
+          text="Try LawBot ↗"
+          size="sm:w-[20vw] md:w-[15vw] lg:w-[10vw] mt-5"
+        />
+      </div>
     </>
-  )
-}
+  );
+};
