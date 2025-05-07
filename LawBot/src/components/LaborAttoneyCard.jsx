1ed5368bbd5eced139debce192481338cd69965f
천지윤 <gunbam715@gmail.com>
2025-05-07 23:56:01 +0900

    feat: 노무사 리스트 ui 다듬기

--- LawBot/src/components/LaborAttoneyCard.jsx
+++ LawBot/src/components/LaborAttoneyCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { LABOR_ATTORNEY_CHAT } from "../constants/path";
 import { Button } from "./Button";
+import { baseURL } from "../constants/baseURL";
 
 export const LaborAttoneyCard = ({ userId, userName, description, photo, index }) => {
   const navigate = useNavigate();
@@ -12,13 +13,13 @@ export const LaborAttoneyCard = ({ userId, userName, description, photo, index }
 
   return (
     <div
-      className={`card card-side bg-base-100 shadow-sm ${
-        index == 0 && "md:col-start-1 md:col-end-3"
-      }
-                ${index == 9 && "md:col-start-2 md:col-end-4"}
-            }`}>
+      className={`card card-side bg-base-100 shadow-sm shrink-0 
+        ${index == 0 && "xl:col-start-1 xl:col-end-3"}
+        ${index == 9 && "xl:col-start-2 xl:col-end-4"}
+      }`}>
       <figure className="shrink-0">
-        <img src={photo || "/src/assets/constant_no_image.png"} alt="Movie" />
+        <img src={photo ? (baseURL + photo) : "/src/assets/constant_no_image.png"} alt="Movie"
+          className="w-[120px] sm:w-[200px] object-contain" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{userName}</h2>
--- LawBot/src/pages/LaborAttorneyList.jsx
+++ LawBot/src/pages/LaborAttorneyList.jsx
@@ -18,7 +18,7 @@ export const LaborAttorneyList = () => {
     <div className="flex flex-col justify-center items-center ">
       <h2 className="text-2xl font-bold mt-10 mb-5 text-center">상담 가능한 노무사</h2>
       <div className="h-[1px] w-20 bg-gray-200 mb-10 "></div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         {list.map((attoney, index) => (
           <LaborAttoneyCard
             userId={attoney.userId}
