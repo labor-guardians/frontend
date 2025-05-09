3c34d5385247b1d95ff91776577a589bf310e245
천지윤 <gunbam715@gmail.com>
2025-05-09 22:51:05 +0900

    feat: 노무사와 대화기록

--- LawBot/src/components/LaborChatHistoryCard.jsx
+++ LawBot/src/components/LaborChatHistoryCard.jsx
@@ -1,32 +1,45 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { LABOR_ATTORNEY_CHAT } from "../constants/path";
-import { Button } from "./Button";
+import { useNavigate } from 'react-router-dom';
+import { LABOR_ATTORNEY_CHAT } from '../constants/path';
+import { Button } from './Button';
+import { BsPersonCircle } from 'react-icons/bs';
+import { baseURL } from '../constants/baseURL';
 
-export const LaborChatHistoryCard = ({ id, name, lastChat, profile }) => {
-    const navigate = useNavigate();
+export const LaborChatHistoryCard = ({ id, title, consultantName, photo }) => {
+  const navigate = useNavigate();
 
-    const goChat = () => {
-        navigate(`/${LABOR_ATTORNEY_CHAT}/${id}`);
-    };
+  const goChat = () => {
+    navigate(`/${LABOR_ATTORNEY_CHAT}/${id}`);
+  };
 
-    return (
-        <div className={"card card-side bg-base-100 shadow-sm w-full max-w-3xl h-[120px]"}>
-            <figure className="shrink-0">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    alt="Movie"
-                />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title ">{name}</h2>
-                <div className="flex flex-row gap-8 ">
-                    <p className="line-clamp-2">{lastChat}</p>
-                    <div className="card-actions w-fit justify-end shrink-0">
-                        <Button onClick={goChat} text="이어서 채팅하기" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      className={
+        'card card-side bg-base-100 shadow-sm w-full  max-w-3xl h-[120px]'
+      }
+    >
+      <figure className="shrink-0">
+        {photo ? (
+          <img
+            src={baseURL + photo}
+            alt="Movie"
+            className="w-[84px]  object-contain"
+          />
+        ) : (
+          <BsPersonCircle
+            color="#A5A5A5"
+            className="w-[84px]  object-contain"
+          />
+        )}
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title ">{consultantName}</h2>
+        <div className="flex flex-row gap-8 ">
+          <p className="line-clamp-2">{title}</p>
+          <div className="card-actions w-fit justify-end shrink-0">
+            <Button onClick={goChat} text="이어서 채팅하기" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
--- LawBot/src/pages/LaborAttorneyChatHistory.jsx
+++ LawBot/src/pages/LaborAttorneyChatHistory.jsx
@@ -1,68 +1,41 @@
-import React, { useState } from "react";
-import { LaborChatHistoryCard } from "../components/LaborChatHistoryCard";
+import { useEffect, useState } from 'react';
+import { LaborChatHistoryCard } from '../components/LaborChatHistoryCard';
+import { apiClient } from '../services/apiClient';
+import useUserData from '../constants/hooks/useUserData';
 
 export const LaborAttorneyChatHistory = () => {
-    const [histories, setHistories] = useState([
-        {
-            id: 1,
-            name: "노무사 이름",
-            lastChat: "마지막 채팅내용",
-            profile: "",
-        },
-        {
-            id: 2,
-            name: "노무사 이름",
-            lastChat:
-                "많이 당황스러우셨겠네요. 우선 회사 측에서 근로자에게 사직서를 강요하는 것은 명백한 불법 행위이며, 근로자는 이를 거부할 권리가 있습니다. 사직서는 본인의 자발적인 의사로 작성해야만 효력이 있기 때문에, 억지로 쓰라고 하는 상황이라면 절대 작성하지 마시고, 해당 대화나 통화 내용은 꼭 증거로 남겨두시는 게 좋아요. 혹시 지금까지 회사로부터 받은 메시지나 녹취 같은 증거 자료가 있으신가요?",
-            profile: "",
-        },
-        {
-            id: 3,
-            name: "노무사 이름",
-            lastChat: "마지막 채팅내용",
-            profile: "",
-        },
-        {
-            id: 4,
-            name: "노무사 이름",
-            lastChat: "마지막 채팅내용",
-            profile: "",
-        },
-        {
-            id: 5,
-            name: "노무사 이름",
-            lastChat: "마지막 채팅내용",
-            profile: "",
-        },
-        {
-            id: 6,
-            name: "노무사 이름",
-            lastChat: "마지막 채팅내용",
-            profile: "",
-        },
-        {
-            id: 7,
-            name: "노무사 이름",
-            lastChat: "마지막 채팅내용",
-            profile: "",
-        },
-    ]);
+  const [histories, setHistories] = useState([]);
+  const { userId } = useUserData();
 
-    return (
-        <div className="flex flex-col justify-center items-center">
-            <h2 className=" text-2xl font-bold mt-10 mb-5 text-center">노무사와 대화기록</h2>
-            <div className="h-[1px] w-20 bg-gray-200 mb-10"></div>
-            <div className="flex flex-col items-center gap-4">
-                {histories.map((history) => (
-                    <LaborChatHistoryCard
-                        id={history.id}
-                        name={history.name}
-                        lastChat={history.lastChat}
-                        profile={history.profile}
-                        key={history.id}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  const loadChatHistory = async () => {
+    const res = await apiClient.get(`/api/conversations/user/${userId}`);
+    console.log(res);
+    setHistories(res.data);
+  };
+
+  useEffect(() => {
+    if (userId) {
+      loadChatHistory();
+    }
+  }, [userId]);
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2 className=" text-2xl font-bold mt-10 mb-5 text-center">
+        노무사와 대화기록
+      </h2>
+      <div className="h-[1px] w-20 bg-gray-200 mb-10"></div>
+      <div className="flex flex-col items-center gap-4 w-full">
+        {histories.map((history) => (
+          <LaborChatHistoryCard
+            id={history.id}
+            title={history.title}
+            consultantName={history.consultant.userName}
+            photo={history.consultant.photo}
+            key={history.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
