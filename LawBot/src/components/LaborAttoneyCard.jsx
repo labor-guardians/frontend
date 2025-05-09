b7cd4aacbcc59f0313acd8ad1263848130df24fe
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2025-05-09 20:47:57 +0900

    feat: 노무사리스트에서 기본 프로필 수정

--- LawBot/src/components/LaborAttoneyCard.jsx
+++ LawBot/src/components/LaborAttoneyCard.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { LABOR_ATTORNEY_CHAT } from "../constants/path";
-import { Button } from "./Button";
-import { baseURL } from "../constants/baseURL";
+import { useNavigate } from 'react-router-dom';
+import { LABOR_ATTORNEY_CHAT } from '../constants/path';
+import { Button } from './Button';
+import { baseURL } from '../constants/baseURL';
+import { BsPersonCircle } from 'react-icons/bs';
 
-export const LaborAttoneyCard = ({ userId, userName, description, photo, index }) => {
+export const LaborAttoneyCard = ({
+  userId,
+  userName,
+  description,
+  photo,
+  index,
+}) => {
   const navigate = useNavigate();
 
   const goChat = () => {
@@ -14,12 +20,23 @@ export const LaborAttoneyCard = ({ userId, userName, description, photo, index }
   return (
     <div
       className={`card card-side bg-base-100 shadow-sm shrink-0 
-        ${index == 0 && "xl:col-start-1 xl:col-end-3"}
-        ${index == 9 && "xl:col-start-2 xl:col-end-4"}
-      }`}>
+        ${index == 0 && 'xl:col-start-1 xl:col-end-3'}
+        ${index == 9 && 'xl:col-start-2 xl:col-end-4'}
+      }`}
+    >
       <figure className="shrink-0">
-        <img src={photo ? (baseURL + photo) : "/src/assets/constant_no_image.png"} alt="Movie"
-          className="w-[120px] sm:w-[200px] object-contain" />
+        {photo ? (
+          <img
+            src={baseURL + photo}
+            alt="Movie"
+            className="w-[120px] sm:w-[200px] object-contain"
+          />
+        ) : (
+          <BsPersonCircle
+            color="#A5A5A5"
+            className="w-[120px] sm:w-[200px] object-contain"
+          />
+        )}
       </figure>
       <div className="card-body">
         <h2 className="card-title">{userName}</h2>
--- LawBot/src/pages/LaborAttorneyList.jsx
+++ LawBot/src/pages/LaborAttorneyList.jsx
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { LaborAttoneyCard } from "../components/LaborAttoneyCard";
-import { apiClient } from "../services/apiClient";
+import { useEffect, useState } from 'react';
+import { LaborAttoneyCard } from '../components/LaborAttoneyCard';
+import { apiClient } from '../services/apiClient';
+import useUserData from '../constants/hooks/useUserData';
 
 export const LaborAttorneyList = () => {
-  const [list, setLust] = useState([]);
+  const [list, setList] = useState([]);
 
   const loadConsultants = async () => {
-    const consultants = await apiClient.get("/api/user/consultant");
-    setLust(consultants.data);
+    const consultants = await apiClient.get('/api/user/consultant');
+    setList(consultants.data);
   };
+
+  const { userId, access, role } = useUserData();
 
   useEffect(() => {
     loadConsultants();
@@ -16,7 +19,9 @@ export const LaborAttorneyList = () => {
 
   return (
     <div className="flex flex-col justify-center items-center ">
-      <h2 className="text-2xl font-bold mt-10 mb-5 text-center">상담 가능한 노무사</h2>
+      <h2 className="text-2xl font-bold mt-10 mb-5 text-center">
+        상담 가능한 노무사
+      </h2>
       <div className="h-[1px] w-20 bg-gray-200 mb-10 "></div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         {list.map((attoney, index) => (
